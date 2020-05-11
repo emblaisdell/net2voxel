5857ae9b4f4f80eb347ce98f77e581eccfc00d6c
Eben Blaisdell <emblaisdell@foresthills.net>
2020-05-11 13:08:15 -0400

    improve texturing

--- texturer.py
+++ texturer.py
@@ -2,28 +2,33 @@
 
 from solid import *
 
-def scadFromBoxes(boxes):
-    obj = union()([ translate(box.corner.tuple())(cube(box.size.tuple())) for box in boxes])
-
-    return obj
+def scadFromBox(box):
+    return translate(box.corner.tuple())(cube(box.size.tuple()))
 
 def texturedModel(boxes, voxelBoundaries, depthFunc):
-    positives = []
-    negatives = []
 
-    for boundary in voxelBoundaries:
-        value = depthFunc(boundary.color)
-        if(value < 0):
-            negatives.append(
-                translate((boundary.voxel.point + ((1.0 + value) * boundary.normal)).tuple())(
-                    cube(1)
-                )
-            )
-        if(value > 0):
-            positives.append(
-                translate((boundary.voxel.point + (value * boundary.normal)).tuple())(
-                    cube(1)
-                )
-            )
+    boxModels = []
 
-    return scadFromBoxes(boxes) + union()(positives) - union()(negatives)
+    for box in boxes:
+        positives = []
+        negatives = []
+
+        for boundary in voxelBoundaries:
+            if boundary.voxel.point in box:
+                value = depthFunc(boundary.color)
+                if(value < 0):
+                    negatives.append(
+                        translate((boundary.voxel.point + ((1.0 + value) * boundary.normal)).tuple())(
+                            cube(1)
+                        )
+                    )
+                if(value > 0):
+                    positives.append(
+                        translate((boundary.voxel.point + (value * boundary.normal)).tuple())(
+                            cube(1)
+                        )
+                    )
+
+        boxModels.append(scadFromBox(box) + union()(positives) - union()(negatives))
+
+    return union()(boxModels)
